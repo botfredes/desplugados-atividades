cdc8d23bd5c7e0875fa2e88164f9c7e61f358e7e
Fredes <fredes@example.com>
2026-03-10 01:35:41 +0000

    Corrigir PostCSS config: usar .cjs com module.exports para compatibilidade

--- react-admin/src/App.tsx
+++ react-admin/src/App.tsx
@@ -1,14 +1,16 @@
 import React from 'react';
-import { Routes, Route } from 'react-router-dom';
+import { HashRouter as Router, Routes, Route } from 'react-router-dom';
 import ActivityList from './components/ActivityList';
 import ActivityDetail from './components/ActivityDetail';
 
 function App() {
   return (
-    <Routes>
-      <Route path="/" element={<ActivityList />} />
-      <Route path="/activity/:id" element={<ActivityDetail />} />
-    </Routes>
+    <Router>
+      <Routes>
+        <Route path="/" element={<ActivityList />} />
+        <Route path="/activity/:id" element={<ActivityDetail />} />
+      </Routes>
+    </Router>
   );
 }
 
